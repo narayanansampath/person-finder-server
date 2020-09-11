1f469f69090c544b2fec8e0cbd80d8221164e2fd
Narayan <sampathnarayanan72@gmail.com>
2020-09-11 16:45:51 -0400

    Updated requirements and google cloud services dependencies

--- cloud_storage/__init__.py
+++ cloud_storage/__init__.py
@@ -3,8 +3,8 @@ from google.cloud import storage
 config_file_path = 'cloud_storage/gcs_storage_key.json'
 
 # buckets
-IMG_PROCESS_OUTPUT_BUCKET = 'image-processing-outputs'
-SEARCHEE_SAMPLE_BUCKET = 'searchee-samples'
-FIR_BUCKET = 'fir-bucket'
+IMG_PROCESS_OUTPUT_BUCKET = 'img_process_output_bucket'
+SEARCHEE_SAMPLE_BUCKET = 'searchee_sample_bucket'
+FIR_BUCKET = 'fir_bucket'
 
 client = storage.Client.from_service_account_json(config_file_path)
